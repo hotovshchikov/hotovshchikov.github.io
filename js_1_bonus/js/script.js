8fd1cd57d231777a118c23c295cb04137e866173
Alexander Hotovshchikov <a.hotovshchikov@gmail.com>
2017-05-05 01:01:38 +0300

    beginning with js

---
+++ js_1_bonus/js/script.js
@@ -0,0 +1,46 @@
+
+
+/*var question = prompt('What do you want to reverse?');
+
+console.log(question.split('').reverse().join(''));*/ //Правильный вариант
+
+/*var arr = question.split('');
+
+for (i = arr.length - 1; i >= 0; i--) {
+console.log(question[i]); 
+} */ //отображает букву с новой строчки
+
+/*var str = prompt('Type anything you want.');
+
+var arr = str.split(" ");
+
+var arrSort = arr.sort();
+
+var longestWord = arrSort[arrSort.length - 1];
+
+var arrLongestWord = [longestWord, longestWord.length];
+
+console.log(arrLongestWord);*/ //отображение массива с длинным словом и кол-вом букв в нем
+
+var userChoice = prompt('Enter a number');
+
+
+function factorial(userChoice) {
+	if (userChoice < 0) {
+		return -1;
+	}
+	else if (userChoice == 0){
+		return 1;
+	}
+	var tmp = userChoice;
+
+	while (userChoice-- > 2) {
+		tmp*= userChoice;
+	}
+
+	return tmp;
+}
+
+var result = factorial(userChoice);
+
+console.log(result);
