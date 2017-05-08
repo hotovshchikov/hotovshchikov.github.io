5790c8fe3a64e73fb7e05dd266c30253ac03c080
Alexander Hotovshchikov <a.hotovshchikov@gmail.com>
2017-05-08 17:44:44 +0300

    bug fixed

--- js_1-2/js/script.js
+++ js_1-2/js/script.js
@@ -3,39 +3,57 @@
 var userChoiceNum = prompt("Enter a number", '');
 var userChoicePow = prompt("Enter a power", '');
 
-if (isNaN(userChoiceNum) && isNuN(userChoicePow)) {
+if (isNaN(userChoiceNum) || isNaN(userChoicePow)) {
 	console.log('lets try again');
 } else {
 	console.log("Result is", mathPow(userChoiceNum, userChoicePow));
 }
 
-function mathPow() {
+function mathPow(userChoiceNum, userChoicePow) {
+  	var originNum = userChoiceNum;
+
+  for (var i = 1; i <= userChoicePow; i++) {
+  	originNum *= userChoiceNum;
+}
+return originNum;
+}
+
+
+/*function mathPow() {
 
   var result = Math.pow(userChoiceNum, userChoicePow);
   
   return result;
-}
+}*/
 
 var userChoiceNames = [];
+arrFill(userChoiceNames);
 
-for (var i = 0; i < 5; i++) {
-  name = prompt("Enter the name");
-  userChoiceNames.push(name);
+function arrFill(userChoiceNames){
+  for (var i = 0; i < 5; i++) {
+    var name = prompt("Enter the name");
+    userChoiceNames.push(name);
+}
+return userChoiceNames;
 }
 
-console.log(userChoiceNames);
+var userLogin = prompt("Enter your login name");
 
-var userLogin = prompt("Enter your login name")
+loginSearch(userLogin);
 
-for (var i = 0; i < userChoiceNames.length; i++) {
+function loginSearch(userLogin){
+  for (var i = 0; i < userChoiceNames.length; i++) {
 	if (userChoiceNames[i] == userLogin) {
 		var match = userLogin;
+
+		if (match) {
+		  return console.log(userLogin + ", вы усешно вошли");
+		}else {
+		return alert("Something goes wrong! Try again!");
+		}
 	}
+  }
 }
 
-if (match) {
-		console.log(userLogin + ", вы усешно вошли");
-	} else {
-		alert("Something goes wrong! Try again!")
-	}
+
 
