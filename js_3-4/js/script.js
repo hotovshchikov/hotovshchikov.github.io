e7eaa08559e2ac7e5ce4b044c512f4043fa356e1
Alexander Hotovshchikov <a.hotovshchikov@gmail.com>
2017-05-21 18:45:59 +0300

    added js

--- js_3-4/js/script.js
+++ js_3-4/js/script.js
@@ -2,17 +2,18 @@
   'use strict';
 
   var test = {
+    addHeader: function() {
+      var header = document.createElement('header');
+
+      header.classList.add('container');
+
+      var wrapper = document.querySelector('.wrapper');
+
+      wrapper.appendChild(header);
+    },
     data: {
       title: 'Тест по какой-то теме',
-      showTitle: function(){
-        var header = document.createElement('header');
-
-        header.classList.add('container');
-
-        var wrapper = document.querySelector('.wrapper');
-
-        wrapper.appendChild(header);
-
+      showTitle: function() {
         var title = document.createElement('h1');
 
         title.classList.add('title');
@@ -23,10 +24,90 @@
 
         headerTitle.appendChild(title);
       },
+      addMain: function() {
+        var main = document.createElement('main');
+
+        main.classList.add('main');
+
+        var mainBox = document.querySelector('.wrapper');
+
+        mainBox.appendChild(main);
+      },
+      addQuestionField: function() {
+        var ol = document.createElement('ol');
+
+        ol.classList.add('question');
+
+        var questionBox = document.querySelector('.main');
+
+        questionBox.appendChild(ol);
+      },
+      addQuestionItem: function() {
+        var li = document.createElement('li');
+
+        li.classList.add('question__item');
+
+        var questionBoxItem = document.querySelector('.question');
+
+        questionBoxItem.appendChild(li);
+      },
+      addQuestionForm: function() {
+        var form = document.createElement('form');
+
+        form.classList.add('numeration');
+
+        var questionBoxForm = document.querySelector('.question__item');
+
+        questionBoxForm.appendChild(form);
+      },
+      addQuestionFieldset: function() {
+        var fieldset = document.createElement('fieldset');
+
+        fieldset.classList.add('fieldset');
+
+        var questionFieldset = document.querySelector('.numeration');
+
+        questionFieldset.appendChild(fieldset);
+      },
+
       questions: [
         {
           title: 'Вопрос #1',
-          answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3']
+          answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3'],
+          showQuestion: function() {
+            var question = document.createElement('legend');
+
+            question.classList.add('title');
+
+            question.innerHTML = this.title;
+
+            var questionTitle = document.querySelector('.fieldset');
+
+            questionTitle.appendChild(question);
+          },
+          showCheckbox: function() {
+            var checkbox = document.createElement('input');
+
+            checkbox.setAttribute('type', 'checkbox');
+
+            checkbox.classList.add('checkbox');
+
+            var answerCheckbox = document.querySelector('.answer__item');
+
+            answerCheckbox.appendChild(checkbox);
+          },
+          showAnswer: function() {
+            var label = document.createElement('label');
+
+            var answerLabel = document.querySelector('.checkbox');
+
+            answerLabel.appendChild(label);
+
+            for (var i = 0, length = this.answers.length; i = length; i++){
+              label.innerHTML = this.answers[i];
+            }
+          }
+
         },
         {
           title: 'Вопрос #2',
@@ -39,5 +120,14 @@
       ]
     }
   };
+  test.addHeader();
   test.data.showTitle();
+  test.data.addMain();
+  test.data.addQuestionField();
+  test.data.addQuestionItem();
+  test.data.addQuestionForm();
+  test.data.addQuestionFieldset();
+  test.questions[0].showQuestion();
+  test.questions[0].showCheckbox();
+  test.questions[0].showAnswer();
 })();
