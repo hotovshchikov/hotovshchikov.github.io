fd92a5443045130d84263f0fc02e315150ba6714
Alexander Hotovshchikov <a.hotovshchikov@gmail.com>
2017-08-09 16:50:17 +0300

    code style fixed/added stop to animation

--- js_9-10/js/main.js
+++ js_9-10/js/main.js
@@ -35,9 +35,9 @@ $(function() {
     // =================MENU===================
     $(document).ready(function() {
         $('.dropdown').hover(function() {
-            $(this).children('.sub-menu').slideDown(400);
+            $(this).stop().children('.sub_menu').stop(true, true).slideDown(400);
         }, function() {
-            $(this).children('.sub-menu').slideUp(400);
+            $(this).stop().children('.sub_menu').stop(true, true).slideUp(400);
         });
     });
 });
